7b4ae63fe7dd22c29708caa3716edf22f9fd2933
swapnali-m <Swapnalimapare@gmail.com>
2021-08-19 15:58:23 +0530

    first commit

---
+++ First_Array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void arr()
+{
+    int icnt=0;
+    int a[]={10,20,30,40};
+    printf("array value is:\n");
+    for(icnt=0;icnt<4;icnt++)
+    {
+       printf(a[0]);
+    }
+    
+}
+int main()
+{
+    arr();
+    return 0;
+}
